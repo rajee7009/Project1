5b917ac614a63cfafb1fd8c05d9fb3d032f224fb
Durairaj_Rajendiran <>
2023-12-19 10:36:17 +0530

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Constants } from 'SharedLib/src/app/_constants/constants';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project1';
+  name = Constants.NAME
 }
